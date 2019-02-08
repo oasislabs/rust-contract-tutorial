bafd9c59b66868faf0d0628a5f34cc7f732e1782
Nick Hynes <nhynes@oasislabs.com>
2019-02-08 21:58:57 +0000

    Use oasis-std

--- contracts/my_counter/src/lib.rs
+++ contracts/my_counter/src/lib.rs
@@ -1,27 +1,21 @@
-#![no_std]
-
-extern crate owasm_std;
-
-use owasm_std::logger::debug;
-
-static COUNTER_KEY: H256 = H256([
+static COUNTER_KEY: [u8; 32] = [
     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-]);
+];
 
 #[owasm_abi_derive::contract]
 trait CounterContract {
     fn constructor(&mut self) {
-        owasm_ethereum::write(&COUNTER_KEY, &U256::zero().into());
+        owasm_ethereum::write(&COUNTER_KEY.into(), &U256::zero().into());
     }
 
     #[constant]
     fn getCount(&mut self) -> U256 {
-        debug("Getting count");
-        U256::from(&owasm_ethereum::read(&COUNTER_KEY))
+        println!("Getting count");
+        U256::from(&owasm_ethereum::read(&COUNTER_KEY.into()))
     }
 
     fn increment(&mut self) {
-        debug("Incrementing count");
-        owasm_ethereum::write(&COUNTER_KEY, &(self.getCount() + 1).into());
+        println!("Incrementing count");
+        owasm_ethereum::write(&COUNTER_KEY.into(), &(self.getCount() + 1).into());
     }
 }
